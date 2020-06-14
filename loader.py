ec879f57a2d564406e477a9c22fcd6e45b3608d0
Nick Pettazzoni <pettazz@gmail.com>
2020-06-14 18:16:37 -0400

    add a git check loop also

--- loader.py
+++ loader.py
@@ -1,9 +1,14 @@
 import logging
 import time
+import pathlib
+
+import git 
 
 import pygame
 
 from hotreload import Loader
+
+GIT_REFRESH_INTERVAL = 300
 
 if __name__ == "__main__":
   logging.basicConfig(filename='clocko.log',
@@ -28,11 +33,20 @@ if __name__ == "__main__":
   last_weather_check = 0
   weather = None
 
+  last_git_check = 0
+  git_repo = git.Repo(pathlib.Path(__file__).parent.absolute())
+
   while True:
+    if time.time() - last_git_check > GIT_REFRESH_INTERVAL:
+      git_repo.remotes.origin.pull()
+      last_git_check = time.time()
+
     new_weather = script.weather_updater(last_weather_check)
     if new_weather is not None:
       last_weather_check = time.time()
       weather = new_weather
       logger.info("got new weather, set last check time to %s" % last_weather_check)
+
     script.run(screen, background, weather)
+    
     time.sleep(1)
