2f514efb40447bbd26810918d3462d1cc485d171
Nick Pettazzoni <pettazz@gmail.com>
2020-06-16 18:21:50 -0400

    use hupper for better reloading

--- loader.py
+++ loader.py
@@ -9,7 +9,7 @@ import pygame
 
 from pyfiglet import Figlet
 
-from hotreload import Loader
+import hupper
 
 import adafruit_dht
 try:
@@ -17,6 +17,8 @@ try:
     DHT_SUPPORT = True
 except NotImplementedError:
     DHT_SUPPORT = False
+
+import runloop
 
 logging.basicConfig(filename='clocko.log',
                     format='%(asctime)s %(name)s [%(levelname)s] %(message)s',
@@ -33,8 +35,6 @@ class Delphi:
     figl = Figlet(font='larry3d')
     banner = figl.renderText("hello delphi")
     logger.info("\n" + banner)
-
-    self.script = Loader("runloop.py")
 
     pygame.init()
 
@@ -77,26 +77,26 @@ class Delphi:
         self.last_git_check = time.time()
         logger.info("set last git pull to %s" % self.last_git_check)
 
-      new_weather = self.script.weather_updater(self.last_weather_check)
+      new_weather = runloop.weather_updater(self.last_weather_check)
       if new_weather is not None:
         self.last_weather_check = time.time()
         weather = new_weather
         logger.info("got new weather, set last check time to %s" % self.last_weather_check)
 
       if DHT_SUPPORT:
-        new_ambient = self.script.ambient_updater(self.last_ambient_check, self.dhtDevice)
+        new_ambient = runloop.ambient_updater(self.last_ambient_check, self.dhtDevice)
         if new_ambient is not None:
           self.last_ambient_check = time.time()
           ambient = new_ambient
           logger.info("got new ambient, set last check time to %s" % self.last_ambient_check)
 
-      new_bg = self.script.background_updater(self.last_background_update)
+      new_bg = runloop.background_updater(self.last_background_update)
       if new_bg is not None:
         self.last_background_update = time.time()
         background_details = new_bg
         logger.info("updating background at %s" % self.last_background_update)
 
-      self.script.run(self.screen, weather, ambient, background_details)
+      runloop.run(self.screen, weather, ambient, background_details)
 
       time.sleep(1)
 
@@ -105,6 +105,16 @@ class Delphi:
     logger.info('caught signal %s, ending runloop' % signum)
     self.alive = False
 
-if __name__ == "__main__":
+def main():
+  reloader = hupper.start_reloader('loader.main')
+  reloader.watch_files([
+    'runloop.py',
+    'statictools.py',
+    'assets/**'
+  ])
+
   d = Delphi()
   d.runner()
+
+if __name__ == "__main__":
+  main()
