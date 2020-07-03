80b320d3fb1d365087953438f4e46bb1514e0cc1
Nick Pettazzoni <pettazz@gmail.com>
2020-07-03 17:29:48 -0400

    RIP dark sky, hello climacell

--- delphi.py
+++ delphi.py
@@ -1,5 +1,5 @@
 import logging
-import time
+import time, datetime
 import pathlib
 import signal
 import sys
@@ -10,9 +10,12 @@ import random
 import requests
 import git 
 import hupper
+from tzlocal import get_localzone
 
 import pygame
 from pygame.locals import *
+
+from PIL import Image
 
 from pyfiglet import Figlet
 
@@ -83,12 +86,150 @@ class Delphi:
     if time.time() - self.last_weather_check > WEATHER_INTERVAL:
         self.logger.info('refreshing weather data...')
         try:
-            res = requests.get(DARKSKY_FORECAST, timeout=1)
+            one_hour_out = (datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=1)).isoformat()
+            CLIMACELL_PARAMS['end_time'] = one_hour_out
+            res = requests.get(CLIMACELL_NOWCAST, timeout=1, params=CLIMACELL_PARAMS)
             if res.status_code == 200:
-                weather = json.loads(res.text)
+                weather_raw = json.loads(res.text)
+
+                with open('lol.json', 'w') as dumperoo:
+                    dumperoo.write(str(weather_raw))
+
+                # basics
+                is_night = datetime.datetime.now(tz=datetime.timezone.utc) > datetime.datetime.fromisoformat(weather_raw[0]['sunset']['value'].replace('Z', '+00:00'))
+                icon = WEATHER_ICONS_MAP['day' if not is_night else 'night'][weather_raw[0]['weather_code']['value']]
+
+                # temp/feels like
+                temp = int(weather_raw[0]['temp']['value'])
+                feels_like = int(weather_raw[0]['feels_like']['value'])
+
+                change_symbol = "+" if feels_like > temp else "-"
+                margin = abs(temp - feels_like)
+                feels_like_indicator = ""
+                if margin > 0:
+                    for interval in range(0, (margin // 5) + 1):
+                        feels_like_indicator = feels_like_indicator + change_symbol
+
+                self.logger.debug("feels like margin of %s, displaying %s" % (margin, feels_like_indicator))
+
+                # next hour status 
+                current = weather_raw[0]['weather_code']['value']
+                inflections = []
+                prev = current
+                for idx, interval in enumerate(weather_raw):
+                    if not interval['weather_code']['value'] == prev:
+                        inflections.append(interval)
+                        prev = interval['weather_code']['value']
+
+                if inflections:
+                    state = "something's gonna happen"
+                else:
+                    state = "%s for the hour" % WEATHER_CODES_TEXT[current]
+
+                # high/low, is this even in the api?
+                high = 90 #int(weather_raw[0]['weather_code']['value'])
+                low = 70 #int(weather_raw[0]['weather_code']['value'])
+
+                # humidity
+                dewpoint = int(weather_raw[0]['dewpoint']['value'])
+                if dewpoint <= 55:
+                    humidity = 'low'
+                elif dewpoint < 65:
+                    humidity = 'medium'
+                else:
+                    humidity = 'high'
+
+                if is_night:
+                    sun_next = 'rise'
+                    sun_time_iso = weather_raw[0]['sunrise']['value']
+                else:
+                    sun_next = 'set'
+                    sun_time_iso = weather_raw[0]['sunset']['value']
+                sun_dt = datetime.datetime.fromisoformat(sun_time_iso.replace('Z', '+00:00')).astimezone(get_localzone())
+                sun_time = sun_dt.strftime("%I:%M")
+
+                if sun_time[0] == "0":
+                    sun_time = sun_time[1:]
+
+                # wind
+                wind_speed = int(weather_raw[0]['wind_speed']['value'])
+                bearing = weather_raw[0]['wind_direction']['value']
+                if bearing > 348.75 or bearing <= 33.75:
+                    wind_direction = 'N'
+                elif bearing > 33.75 and bearing <= 78.75:
+                    wind_direction = 'NE'
+                elif bearing > 78.75 and bearing <= 123.75:
+                    wind_direction = 'E'
+                elif bearing > 123.75 and bearing <= 168.75:
+                    wind_direction = 'SE'
+                elif bearing > 168.75 and bearing <= 213.75:
+                    wind_direction = 'S'
+                elif bearing > 213.75 and bearing <= 258.75:
+                    wind_direction = 'SW'
+                elif bearing > 258.75 and bearing <= 303.75:
+                    wind_direction = 'W'
+                elif bearing > 303.75 and bearing <= 348.75:
+                    wind_direction = 'NW'
+
+                # air quality
+                aqi = weather_raw[0]['epa_aqi']['value']
+                if aqi <= 50:
+                    air_quality = 'good'
+                    air_quality_color = 'green'
+                elif aqi <= 100:
+                    air_quality = 'moderate'
+                    air_quality_color = 'yellow'
+                elif aqi <= 150:
+                    air_quality = 'bad'
+                    air_quality_color = 'orange'
+                elif aqi <= 200:
+                    air_quality = 'unhealthy'
+                    air_quality_color = 'red'
+                elif aqi <= 300:
+                    air_quality = 'very unhealthy'
+                    air_quality_color = 'purple'
+                else:
+                    air_quality = 'hazardous'
+                    air_quality_color = 'maroon'
+                
+                # pollen
+                tree_pollen = weather_raw[0]['pollen_tree']['value']
+                tree_pollen = tree_pollen if tree_pollen is not None else 0
+                tree_pollen_color = POLLEN_COLOR_SCALE[tree_pollen]
+
+                weed_pollen = weather_raw[0]['pollen_weed']['value']
+                weed_pollen = weed_pollen if weed_pollen is not None else 0
+                weed_pollen_color = POLLEN_COLOR_SCALE[weed_pollen]
+
+                grass_pollen = weather_raw[0]['pollen_grass']['value']
+                grass_pollen = grass_pollen if grass_pollen is not None else 0
+                grass_pollen_color = POLLEN_COLOR_SCALE[grass_pollen]
+
+                weather = {
+                    'temp': temp,
+                    'is_night': is_night,
+                    'icon': icon,
+                    'feels_like': feels_like,
+                    'feels_like_indicator': feels_like_indicator,
+                    'state': state,
+                    'high': high,
+                    'low': low,
+                    'dewpoint': dewpoint,
+                    'humidity': humidity,
+                    'sun_time': sun_time,
+                    'sun_next': sun_next,
+                    'wind_speed': wind_speed,   
+                    'wind_direction': wind_direction,
+                    'air_quality': air_quality,
+                    'air_quality_color': air_quality_color,
+                    'tree_pollen_color': tree_pollen_color,
+                    'weed_pollen_color': weed_pollen_color,
+                    'grass_pollen_color': grass_pollen_color
+                }
+
                 self.last_weather_check = time.time()
 
-                self.logger.info("successfully fetched new weather, timestamp: %s" % weather['currently']['time'])
+                self.logger.info("successfully fetched new weather, current time: %s" % weather_raw[0]['observation_time']['value'])
                 self.logger.info("got new weather, set last check time to %s" % self.last_weather_check)
         except Exception as e:
             self.logger.warning("failed to fetch weather, guess we'll try next tick", exc_info=True)
@@ -117,7 +258,12 @@ class Delphi:
               self.logger.warning("failed to fetch ambient, guess we'll try next tick")
               self.logger.warning(e)
     else:
-      self.logger.info("DHT support disabled.")
+
+      ambient = {
+          "temperature": "{:.1f}".format(80),
+          "humidity": "27"
+      }
+      self.logger.info("DHT support disabled, using fake data")
 
     return ambient
 
@@ -137,11 +283,11 @@ class Delphi:
 
         img_width, img_height = img.get_size()
         if round(img_width / img_height, 3) > 0.6:
-          new_height = SCREEN_HEIGHT
-          new_width = ((new_height * img_width) // img_height) + SCREEN_WIDTH_CORRECTION
+            new_height = SCREEN_HEIGHT
+            new_width = ((new_height * img_width) // img_height) + SCREEN_WIDTH_CORRECTION
         else:
-          new_width = SCREEN_WIDTH + SCREEN_WIDTH_CORRECTION
-          new_height = (new_width * img_height) // img_width
+            new_width = SCREEN_WIDTH + SCREEN_WIDTH_CORRECTION
+            new_height = (new_width * img_height) // img_width
 
         background = pygame.transform.smoothscale(img, (new_width, new_height))
       
@@ -160,6 +306,7 @@ class Delphi:
     return background_details
 
   def draw_screen(self, weather, ambient, background):
+    text_color = (255, 255, 255)
     if background is not None:
         self.screen.blit(background['image'], (background['offset'][0], background['offset'][1]))
 
@@ -169,28 +316,63 @@ class Delphi:
     if now_time[0] == "0":
         now_time = now_time[1:]
 
-    text_shadow(self.screen, now_time, (242, 202), 200, (255, 255, 255))
-    text_shadow(self.screen, now_date, (242, 302), 45, (255, 255, 255))
-
-    if ambient is not None:
-        fa_text_shadow(self.screen, 'tachometer-alt', (372, 472), 65, (255, 255, 255), "left")
-        text_shadow(self.screen, "Temperature: %s°F" % ambient['temperature'], (52, 472), 30, (255, 255, 255), "left")
-        text_shadow(self.screen, "Humidity: %s%%" % ambient['humidity'], (52, 502), 30, (255, 255, 255), "left")
+    header_shadowed(self.screen, now_time, (242, 132), 210, text_color)
+    header_shadowed(self.screen, now_date, (242, 232), 45, text_color)
 
     if weather is not None:
-        icon = WEATHER_ICON_MAP[weather['currently']['icon']]
-        temp = int(weather['currently']['temperature'])
-        feels = int(weather['currently']['apparentTemperature'])
-        precipProb = int(weather['currently']['precipProbability'] * 100)
-        hour_summary = weather['minutely']['summary']
+        # main
+        fa_text_shadowed(self.screen, weather['icon'], (175, 507), 90, text_color)
+        header_shadowed(self.screen, str(weather['temp']) + "°", (305, 510), 100, text_color)
+        body_text_shadowed(self.screen, weather['feels_like_indicator'], (340, 500), 35, text_color)
+        header_shadowed(self.screen, weather['state'], (242, 575), 35, text_color)
 
-        text1 = "%s°, feels like %s°, %s%% precip" % (temp, feels, precipProb)
-        text2 = hour_summary
+        # hi/low
+        fa_text_shadowed(self.screen, 'angle-up', (120, 603), 23, text_color, "left")
+        body_text_shadowed(self.screen, str(weather['high']) + "°", (137, 600), 25, text_color)
+        fa_text_shadowed(self.screen, 'angle-down', (190, 603), 23, text_color, "left")
+        body_text_shadowed(self.screen, str(weather['low']) + "°", (207, 600), 25, text_color)
 
-        fa_text_shadow(self.screen, icon, (32, 572), 65, (255, 255, 255), "left")
-        text_shadow(self.screen, text1, (122, 572), 30, (255, 255, 255), "left")
-        text_shadow(self.screen, text2, (122, 602), 30, (255, 255, 255), "left")
+        # dewpoint
+        fa_text_shadowed(self.screen, 'tint', (280, 603), 23, text_color, "left")
+        body_text_shadowed(self.screen, str(weather['dewpoint']) + "°", (305, 600), 25, text_color)
+        if weather['humidity'] == 'low':
+            humidity_icon = 'smile'
+        elif weather['humidity'] == 'medium':
+            humidity_icon = 'meh'
+        elif weather['humidity'] == 'high':
+            humidity_icon = 'frown-open'
+        fa_text_shadowed(self.screen, humidity_icon, (345, 603), 23, text_color, "left")
 
+        # sunrise/set
+        if weather['sun_next'] == 'rise':
+            sun_icon = 'sun'
+        else:
+            sun_icon = 'moon'
+        fa_text_shadowed(self.screen, sun_icon, (120, 640), 20, text_color, "left")
+        body_text_shadowed(self.screen, weather['sun_time'], (145, 635), 25, text_color)
+
+        # wind
+        fa_text_shadowed(self.screen, 'wind', (260, 640), 20, text_color, "left")
+        body_text_shadowed(self.screen, str(weather['wind_speed']), (293, 650), 25, text_color, "center")
+        body_text_shadowed(self.screen, " mph", (303, 641), 17, text_color)
+        body_text_shadowed(self.screen, str(weather['wind_direction']), (340, 635), 25, text_color) # an icon someday
+
+        # pollen / aqi
+        fa_text_shadowed(self.screen, 'tree', (120, 680), 20, text_color, "left")
+        fa_text_shadowed(self.screen, 'dot-circle', (137, 680), 20, COLOR_RGB[weather['tree_pollen_color']], "left")
+        fa_text_shadowed(self.screen, 'leaf', (170, 680), 20, text_color, "left")
+        fa_text_shadowed(self.screen, 'dot-circle', (197, 680), 20, COLOR_RGB[weather['weed_pollen_color']], "left")
+        fa_text_shadowed(self.screen, 'seedling', (230, 680), 20, text_color, "left")
+        fa_text_shadowed(self.screen, 'dot-circle', (253, 680), 20, COLOR_RGB[weather['grass_pollen_color']], "left")
+
+        # aqi
+        fa_text_shadowed(self.screen, 'industry', (325, 680), 20, text_color, "left")
+        fa_text_shadowed(self.screen, 'dot-circle', (348, 680), 20, COLOR_RGB[weather['air_quality_color']], "left")
+
+    if ambient is not None:
+        fa_text_shadowed(self.screen, 'microchip', (170, 733), 20, text_color, "left")
+        body_text_shadowed(self.screen, "%s° /  %s%%" % (ambient['temperature'], ambient['humidity']), (200, 730), 25, text_color)
+        
     pygame.display.flip()
 
   def runner(self):
@@ -205,6 +387,9 @@ class Delphi:
           if event.type == KEYDOWN:
               if event.key == K_ESCAPE:
                   self.quitter(msg='ESC key')
+              if event.key == K_RIGHT:
+                  self.last_background_update = 0
+                  self.logger.info('next background...')
 
       if time.time() - self.last_git_check > GIT_REFRESH_INTERVAL:
         self.logger.info("updating git repo...")
@@ -216,9 +401,9 @@ class Delphi:
       if new_weather is not None:
         weather = new_weather
 
-      if DHT_SUPPORT:
-        new_ambient = self.ambient_updater()
-        if new_ambient is not None:
+      
+      new_ambient = self.ambient_updater()
+      if new_ambient is not None:
           ambient = new_ambient
           
       new_bg = self.background_updater()
--- statictools.py
+++ statictools.py
@@ -1,29 +1,138 @@
 import logging
 
+from secrets import *
+
 import fontawesome as fa
 
 import pygame
 from pygame.locals import *
 
+from PIL import Image
+
 LOGLEVEL = logging.DEBUG
+
+COLOR_RGB = {
+  'red': (255, 0, 0),
+  'orange': (255, 85, 0),
+  'yellow-orange': (255, 180, 0),
+  'yellow': (255, 255, 0),
+  'yellow-green': (180, 255, 0),
+  'green': (0, 255, 0),
+  'purple': (128, 0, 128),
+  'maroon': (128, 0, 0)
+}
+
+POLLEN_COLOR_SCALE = {
+  0: 'green',
+  1: 'yellow-green',
+  2: 'yellow',
+  3: 'yellow-orange',
+  4: 'orange',
+  5: 'red'
+}
 
 BACKGROUND_INTERVAL = 300
 BACKGROUND_PATH = "assets/img/backgrounds/"
-WEATHER_INTERVAL = 300
 AMBIENT_INTERVAL = 120
-DARKSKY_FORECAST = 'https://api.darksky.net/forecast/fd361ec0a4d4d24011a96cb6b47cd272/42.378920,-71.091040'
-WEATHER_ICON_MAP = {
-  'clear-day': 'sun',
-  'clear-night': 'moon',
-  'rain': 'cloud-rain',
-  'snow': 'snowflake',
-  'sleet': 'cloud-showers-heavy',
-  'wind': 'wind',
-  'fog': 'smog',
-  'cloudy': 'cloud',
-  'partly-cloudy-day': 'cloud-sun',
-  'partly-cloudy-night': 'cloud-moon'
+WEATHER_INTERVAL = 120
+CLIMACELL_FIELDS = [
+  'precipitation',
+  'precipitation_type',
+  'temp',
+  'feels_like',
+  'dewpoint',
+  'wind_speed',
+  'wind_direction',
+  'sunrise',
+  'sunset',
+  'weather_code',
+  'pollen_tree',
+  'pollen_grass',
+  'pollen_weed',
+  'epa_aqi'
+]
+CLIMACELL_PARAMS = {
+  'apikey': CLIMACELL_API_KEY,
+  'lat': LATLONG[0],
+  'lon': LATLONG[1],
+  'unit_system': 'us',
+  'timestep': 5,
+  'fields': CLIMACELL_FIELDS
 }
+CLIMACELL_NOWCAST  = 'https://api.climacell.co/v3/weather/nowcast'
+WEATHER_CODES_TEXT = {
+  'rain_heavy': 'heavy rain',
+  'rain': 'rain',
+  'freezing_rain_heavy': 'heavy freezing rain',
+  'freezing_rain': 'freezing rain',
+  'freezing_rain_light': 'light freezing rain',
+  'freezing_drizzle': 'freezing drizzle',
+  'ice_pellets_heavy': 'heavy ice',
+  'ice_pellets': 'ice',
+  'ice_pellets_light': 'light ice',
+  'snow_heavy': 'heavy snow',
+  'snow': 'snow',
+  'snow_light': 'light snow',
+  'flurries': 'flurries',
+  'tstorm': 'thunderstorm',
+  'fog_light': 'light fog',
+  'fog': 'fog',
+  'cloudy': 'cloudy',
+  'mostly_cloudy': 'mostly cloudy',
+  'partly_cloudy': 'partly cloudy',
+  'mostly_clear': 'mostly clear',
+  'clear': 'clear',
+  'rain_light': 'light rain',
+  'drizzle': 'drizzle'
+}
+WEATHER_ICONS_MAP = {
+  'base': {
+    'rain_heavy': 'cloud-rain',
+    'rain': 'cloud-rain',
+    'freezing_rain_heavy': 'cloud-showers-heavy',
+    'freezing_rain': 'cloud-showers-heavy',
+    'freezing_rain_light': 'cloud-showers-heavy',
+    'freezing_drizzle': 'cloud-rain',
+    'ice_pellets_heavy': 'cloud-showers-heavy',
+    'ice_pellets': 'cloud-showers-heavy',
+    'ice_pellets_light': 'cloud-showers-heavy',
+    'snow_heavy': 'snowflake',
+    'snow': 'snowflake',
+    'snow_light': 'snowflake',
+    'flurries': 'snowflake',
+    'tstorm': 'bolt',
+    'fog_light': 'smog',
+    'fog': 'smog',
+    'cloudy': 'cloud',
+    'mostly_cloudy': 'cloud',
+    'partly_cloudy': 'cloud',
+    'mostly_clear': 'cloud',
+    'clear': 'sun',
+    'rain_light': 'cloud-rain',
+    'drizzle': 'cloud-rain'
+  }
+}
+
+WEATHER_ICONS_DAY = {
+  'mostly_cloudy': 'cloud-sun',
+  'partly_cloudy': 'cloud-sun',
+  'mostly_clear': 'cloud-sun',
+  'clear': 'sun',
+  'rain_light': 'cloud-sun-rain',
+  'drizzle': 'cloud-sun-rain'
+}
+
+WEATHER_ICONS_NIGHT = {
+  'mostly_cloudy': 'cloud-moon',
+  'partly_cloudy': 'cloud-moon',
+  'mostly_clear': 'cloud-moon',
+  'clear': 'moon',
+  'rain_light': 'cloud-moon-rain',
+  'drizzle': 'cloud-moon-rain'
+}
+
+WEATHER_ICONS_MAP['day'] = {**WEATHER_ICONS_MAP['base'], **WEATHER_ICONS_DAY}
+WEATHER_ICONS_MAP['night'] = {**WEATHER_ICONS_MAP['base'], **WEATHER_ICONS_NIGHT}
 
 def _textgen(screen, text, position, size, color, align, font):
     text = font.render(text, 1, color)
@@ -36,22 +145,30 @@ def _textgen(screen, text, position, size, color, align, font):
         textpos.center = position
     screen.blit(text, textpos)
 
-def screen_text(screen, text, position, size, color, align="center"):
+def header(screen, text, position, size, color, align="center"):
     font = pygame.font.Font('assets/font/Staatliches-Regular.ttf', size)
     _textgen(screen, text, position, size, color, align, font)
 
-def text_shadow(screen, text, position, size, color, align="center", shadow_color=(0, 0, 0)):
-    screen_text(screen, text, position, size, shadow_color, align)
-    screen_text(screen, text, (position[0] - 3, position[1] - 3), size, color, align)
+def header_shadowed(screen, text, position, size, color, align="center", shadow_color=(0, 0, 0)):
+    header(screen, text, position, size, shadow_color, align)
+    header(screen, text, (position[0] - 2, position[1] - 2), size, color, align)
+
+def body_text(screen, text, position, size, color, align="left"):
+    font = pygame.font.Font('assets/font/Staatliches-Regular.ttf', size)
+    _textgen(screen, text, position, size, color, align, font)
+
+def body_text_shadowed(screen, text, position, size, color, align="left", shadow_color=(0, 0, 0)):
+    body_text(screen, text, position, size, shadow_color, align)
+    body_text(screen, text, (position[0] - 2, position[1] - 2), size, color, align)
 
 def fa_text(screen, name, position, size, color, align="center"):
     font = pygame.font.Font('assets/font/fa-solid-900.ttf', size)
     _textgen(screen, fa.icons[name], position, size, color, align, font)
 
-def fa_text_shadow(screen, name, position, size, color, align="center", shadow_color=(0, 0, 0)):
+def fa_text_shadowed(screen, name, position, size, color, align="center", shadow_color=(0, 0, 0)):
     fa_text(screen, name, position, size, shadow_color, align)
-    fa_text(screen, name, (position[0] - 3, position[1] - 3), size, color, align)
+    fa_text(screen, name, (position[0] - 2, position[1] - 2), size, color, align)
 
 def fullscreen_message(screen, message, color):
     screen.fill((0, 0, 0))
-    screen_text(screen, message, (240, 400), 30, color)
+    header(screen, message, (240, 400), 30, color)
